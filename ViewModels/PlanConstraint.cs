48e4fd7690fb609a2ff90576f30dbf1132b90572
Chingyun Cheng <cc1897@cinj.rutgers.edu>
2022-08-17 09:59:31 -0700

    working prototype for DVH prostate

---
+++ ViewModels/PlanConstraint.cs
@@ -0,0 +1,33 @@
+﻿
+
+using ESAPIX.Constraints;
+using Prism.Mvvm;
+
+namespace ESAPX_StarterUI.ViewModels
+{
+    public class PlanConstraint : BindableBase
+    {
+
+
+        public PlanConstraint(IConstraint constraint)
+        {
+            this.constraint = constraint;
+        }
+
+        private IConstraint _constraint;
+        public IConstraint Constraint
+        {
+            get { return _constraint; }
+            set { SetProperty(ref _constraint, value); }
+        }
+
+        private ConstraintResult _result;
+        private IConstraint constraint;
+
+        public ConstraintResult Result
+        {
+            get { return _result; }
+            set { SetProperty(ref _result, value); }
+        }
+    }
+}
